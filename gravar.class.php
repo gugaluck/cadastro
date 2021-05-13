479955f64c65449dd1e7afd82e1525b2a645a85e
gugaluck <53919975+gugaluck@users.noreply.github.com>
2021-05-13 16:16:04 -0300

    Update gravar.class.php

--- gravar.class.php
+++ gravar.class.php
@@ -2,12 +2,15 @@
     class Gravar {
         private $arquivo = 'cadastro.txt';
         private $dados;
+        private $nome;
         
         public function __construct() {  
             $this->dados = fopen($this->arquivo, 'r');
-            $retorno = file_put_contents('cadastro.txt', '123456789123456789', FILE_USE_INCLUDE_PATH);
+            $this->nome = $_REQUEST['nome'];
+            $retorno = file_put_contents('cadastro.txt', $this->nome, FILE_USE_INCLUDE_PATH);
             echo $retorno;
         }
     }
+    
 
     $b = new Gravar;
