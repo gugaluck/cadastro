985af7699219f4c6e53c376ccd1a385a58e86ad0
gugaluck <53919975+gugaluck@users.noreply.github.com>
2021-05-19 10:50:01 -0300

    Update gravar.class.php

--- gravar.class.php
+++ gravar.class.php
@@ -3,11 +3,18 @@
         private $arquivo = 'cadastro.txt';
         private $dados;
         private $nome;
+        private $retorno;
+
         
-        public function __construct($arquivo) {  
-            $this->$dados = fopen($this->$arquivo, 'a+');
-            //$this->nome = $_POST['nome'];
-            $this->$retorno = file_put_contents($arquivo, 'teste'."\n", FILE_APPEND);
+        public function __construct($arquivo = "cadastro.txt") {  
+            if(file_exists($arquivo)){
+                $this->$dados = fopen($this->$arquivo, "a+");
+                $this->$nome = $_POST['nome'];
+                $this->$retorno = file_put_contents($arquivo, $_POST['nome']."\n", FILE_APPEND);
+                echo $this->$retorno;
+            }
+            else 
+                echo 'Arquivo não existe';
         }
     }  
     $g = new Gravar;
