f1fbcbab9e7ba468bd1a2777f8c3e26bf68c60d4
gugaluck <53919975+gugaluck@users.noreply.github.com>
2021-05-19 10:50:03 -0300

    Update ler.class.php

--- ler.class.php
+++ ler.class.php
@@ -4,9 +4,8 @@
         private $dados;
         private $linha;
 
-        public function __construct($arquivo) {  
+        public function __construct($arquivo = 'cadastro.txt') {  
             $this->$dados = fopen($this->$arquivo, 'a+');
-            
             while (!feof($this->$dados)) {
                 $this->$linha = fgets($this->$arquivo, 1024);
                 echo $this->$linha.'<br />';
