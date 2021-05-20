90ee72cbe427e8701ce8f1451f44ed5d5d626799
gugaluck <53919975+gugaluck@users.noreply.github.com>
2021-05-20 17:13:32 -0300

    Update gravar.class.php

--- gravar.class.php
+++ gravar.class.php
@@ -2,12 +2,16 @@
     class Gravar {
         private $arquivo = 'cadastro.txt';
         private $dados;
-        private $nome;
+        private $array;
         private $retorno;
 
         public function __construct() {
             $this->dados = fopen($this->arquivo, "a+");
+            
+            //return $this->dados;
             $this->retorno = file_put_contents('cadastro.txt', $_POST['nome'], FILE_APPEND);
+            //$this->array = explode('|', $this->dados);
+            //$this->explode = explode('|', $this->retorno);
             $this->retorno = file_put_contents('cadastro.txt', $_POST['valor'], FILE_APPEND);
             $this->retorno = file_put_contents('cadastro.txt', $_POST['qtdestoque']."\n", FILE_APPEND);
             echo $this->retorno;
