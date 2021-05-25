b5d0cd4e7864a3aa9dcf0b10bfbd60ed2950ab23
gugaluck <53919975+gugaluck@users.noreply.github.com>
2021-05-25 15:37:46 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <html>
+    <link href="index.css" rel="stylesheet">
     <h1>Sistema de cadastro em arquivo .txt</h1> 
-    <form method="POST" action="gravar.class.php">
-        <input type="text" name="nome" placeholder="Digite o nome" /><br>
-        <input type="text" name="valor" placeholder="Digite o valor" /><br>
-        <input type="text" name="qtdestoque" placeholder="Digite o quantidade em estoque" /><br>
-        <input type="submit" value="Enviar">    
-    </form>
+    <div>
+        <form method="POST" action="gravar.class.php">
+            <input type="text" name="nome" placeholder="Nome"/>
+            <input type="text" name="valor" placeholder="Valor"/>
+            <input type="text" name="qtdestoque" placeholder="Quantidade em estoque"/>
+            <input type="submit" value="Enviar">   
+        </form>
+    </div>
 </html>
 
 <?php 
