9e8af15e256de4316112122848fd39b4d1f82613
gugaluck <53919975+gugaluck@users.noreply.github.com>
2021-05-25 15:37:37 -0300

    Update gravar.class.php

--- gravar.class.php
+++ gravar.class.php
@@ -8,8 +8,7 @@
         public function __construct() {
             $this->dados = fopen($this->arquivo, "a+");
             $this->retorno = file_put_contents($this->arquivo, $_POST['nome'], FILE_APPEND);
-            //$this->array = explode('|', $this->dados);
-            //$this->retorno = explode('|', $this->retorno);
+            //$this->array = explode('|', $this->retorno);
             $this->retorno = file_put_contents($this->arquivo, $_POST['valor'], FILE_APPEND);
             $this->retorno = file_put_contents($this->arquivo, $_POST['qtdestoque']."\n", FILE_APPEND);
             echo $this->retorno;
