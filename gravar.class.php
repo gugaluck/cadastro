8fc8759e597f4fc37b419ddfc1c3691b44854b65
gugaluck <53919975+gugaluck@users.noreply.github.com>
2021-05-26 09:28:19 -0300

    Update gravar.class.php

--- gravar.class.php
+++ gravar.class.php
@@ -7,10 +7,24 @@
 
         public function __construct() {
             $this->dados = fopen($this->arquivo, "a+");
-            $this->retorno = file_put_contents($this->arquivo, $_POST['nome'], FILE_APPEND);
-            //$this->array = explode('|', $this->retorno);
-            $this->retorno = file_put_contents($this->arquivo, $_POST['valor'], FILE_APPEND);
-            $this->retorno = file_put_contents($this->arquivo, $_POST['qtdestoque']."\n", FILE_APPEND);
+            $this->nome = $_POST['nome'];
+            $this->valor = $_POST['valor'];
+            $this->qtdestoque = $_POST['qtdestoque'];
+            if ($_POST['nome'] == "empty") {
+                echo "<p> Você não digitou todos os campos! </p>";
+                exit();
+            } elseif ($_POST['valor'] == "empty") {
+                echo "<p> Você não digitou todos os campos! </p>";
+                exit();
+            } elseif ($_POST['qtdestoque'] == "empty") {
+                echo "<p> Você não digitou todos os campos! </p>";
+                exit();
+            } else {
+                $this->retorno = file_put_contents($this->arquivo, $_POST['nome'], FILE_APPEND);
+                //$this->array = explode('|', $this->retorno);
+                $this->retorno = file_put_contents($this->arquivo, $_POST['valor'], FILE_APPEND);
+                $this->retorno = file_put_contents($this->arquivo, $_POST['qtdestoque']."\n", FILE_APPEND);
+            }
             echo $this->retorno;
             header('Location: index.php');
         
