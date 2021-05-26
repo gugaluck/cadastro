9b23a21d9c4a655c6450c72dd375f17629c42213
gugaluck <53919975+gugaluck@users.noreply.github.com>
2021-05-26 11:15:59 -0300

    Update gravar.class.php
    levei umas 3 hrs pra conseguir fazer dar certo o alerta kkkkkkk

--- gravar.class.php
+++ gravar.class.php
@@ -2,32 +2,20 @@
     class Gravar {
         private $arquivo = 'cadastro.txt';
         private $dados;
-        private $array;
-        private $retorno;
 
         public function __construct() {
             $this->dados = fopen($this->arquivo, "a+");
             $this->nome = $_POST['nome'];
             $this->valor = $_POST['valor'];
             $this->qtdestoque = $_POST['qtdestoque'];
-            if ($_POST['nome'] == "empty") {
-                echo "<p> Você não digitou todos os campos! </p>";
-                exit();
-            } elseif ($_POST['valor'] == "empty") {
-                echo "<p> Você não digitou todos os campos! </p>";
-                exit();
-            } elseif ($_POST['qtdestoque'] == "empty") {
-                echo "<p> Você não digitou todos os campos! </p>";
-                exit();
-            } else {
-                $this->retorno = file_put_contents($this->arquivo, $_POST['nome'], FILE_APPEND);
-                //$this->array = explode('|', $this->retorno);
-                $this->retorno = file_put_contents($this->arquivo, $_POST['valor'], FILE_APPEND);
-                $this->retorno = file_put_contents($this->arquivo, $_POST['qtdestoque']."\n", FILE_APPEND);
-            }
-            echo $this->retorno;
-            header('Location: index.php');
-        
+            
+            if ((!empty($this->nome)) && (!empty($this->valor)) && (!empty($this->valor))) {
+                $this->dados = file_put_contents($this->arquivo, $_POST['nome'], FILE_APPEND);
+                $this->dados = file_put_contents($this->arquivo, $_POST['valor'], FILE_APPEND);
+                $this->dados = file_put_contents($this->arquivo, $_POST['qtdestoque']."\n", FILE_APPEND);
+                header('Location: index.php');
+            } else
+                echo "<script>window.location='index.php';alert('Insira todos os dados');</script>";
         }
     }  
     $g = new Gravar();
